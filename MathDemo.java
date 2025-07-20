5f322da85a234ecab631f456d63dd751d09e3685
Tasneem Ibrahim <tasneem@example.com>
2025-07-20 12:04:24 +0400

    add fun2

---
+++ MathDemo.java
@@ -0,0 +1,17 @@
+public class MathDemo {
+
+    //task 2
+    public static void main(String[] args) {
+        double a = 9;
+        double b = 2;
+
+        System.out.println("Square root of a: " + Math.sqrt(a));
+        System.out.println("a raised to b: " + Math.pow(a, b));
+        System.out.println("Absolute value of -15: " + Math.abs(-15));
+        System.out.println("Log base 10 of 100: " + Math.log10(100));
+        System.out.println("Ceil of 4.3: " + Math.ceil(4.3));
+        System.out.println("Floor of 4.7: " + Math.floor(4.7));
+        System.out.println("Sin of 90°: " + Math.sin(Math.toRadians(90)));
+        System.out.println("Max of 5 and 10: " + Math.max(5, 10));
+    }
+}
